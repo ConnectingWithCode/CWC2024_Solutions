2efbea5bc0958cf0d745ca13831fa61aaa1af1a1
BobSchaffer1 <bob.schaffer@gmail.com>
2023-11-16 12:06:01 -0800

    Added Reset Game features - may have bug

--- LinearLightsOut/main.py
+++ LinearLightsOut/main.py
@@ -7,15 +7,14 @@ game.game_over = False
 
 play.new_text("Linear Lights Out",
               y=200,
-              font_size=100,
-              color='yellow')
+              font_size=100)
 
-### Reminder for 11/16 - change yellow, maybe update start game to be random presses
+### Reminder for 11/30 - revisit reset_game - consider easier version
 
 message = play.new_text("Turn all of the lights off!",
                         y=100,
                         font_size=50,
-                        color='yellow')
+                        color = 'grey')
 
 light_list = []
 light_images = ["light_off.png", "light_on.png"]
@@ -25,7 +24,7 @@ for k in range(7):
     light = play.new_image(light_images[0],
                            x=-300 + k * 100,
                            y=0,
-                           size=50)
+                           size=75)
     light.is_on = False
     light.num = k
     light_list.append(light)
@@ -36,14 +35,38 @@ def reset_game():
     game.game_over = False
     message.words = "Turn all of the lights off!"
     for k in range(7):
-        if random.random() < 0.5:
-            light_list[k].image = light_images[0]
-            light_list[k].is_on = False
-        else:
-            light_list[k].image = light_images[1]
-            light_list[k].is_on = True
+        light_list[k].image = light_images[0]
+        light_list[k].is_on = False
+    # for _ in range(100):
+    #     button_press(random.randint(0,6))
+    update_game_over()
+    if game.game_over:
+        button_press(random.randint(0,6))
+        update_game_over()
 
-reset_game()
+
+def button_press(light_num):
+    if light_num == 0:
+        toggle_light(light_list[light_num])
+        toggle_light(light_list[light_num + 1])
+    elif light_num == 6:
+        toggle_light(light_list[light_num - 1])
+        toggle_light(light_list[light_num])
+    else:
+        toggle_light(light_list[light_num - 1])
+        toggle_light(light_list[light_num])
+        toggle_light(light_list[light_num + 1])
+
+
+def update_game_over():
+    off_counter = 0
+    for light in light_list:
+        if not light.is_on:
+            off_counter += 1
+    if off_counter == len(light_list):
+        game.game_over = True
+    else
+        game.game_over = False
 
 @play.repeat_forever
 async def control_lights():
@@ -52,36 +75,20 @@ async def control_lights():
         if game.counter == 1:
             message.words = f"You won in {game.counter} move!"
         message.show()
-        await play.timer(seconds=5)
+        await play.timer(seconds=2)
+        message.words = f"New game about to start."
+        await play.timer(seconds=2)
         reset_game()
         return
     for light in light_list:
         if play.mouse.is_clicked:
             if play.mouse.is_touching(light):
-                if light.num == 0:
-                    toggle_light(light_list[light.num])
-                    toggle_light(light_list[light.num + 1])
-                    game.counter += 1
-                elif light.num == 6:
-                    toggle_light(light_list[light.num - 1])
-                    toggle_light(light_list[light.num])
-                    game.counter += 1
-                else:
-                    toggle_light(light_list[light.num - 1])
-                    toggle_light(light_list[light.num])
-                    toggle_light(light_list[light.num + 1])
-                    game.counter += 1
+                button_press(light.num)
+                game.counter += 1
                 message.words = f"Moves: {game.counter}"
                 await play.timer(seconds=0.25)
-
-
     off_counter = 0
-    for light in light_list:
-        if not light.is_on:
-            off_counter += 1
-
-    if off_counter == len(light_list):
-        game.game_over = True
+    update_game_over()
 
 
 def toggle_light(light):
@@ -93,5 +100,6 @@ def toggle_light(light):
         light.is_on = True
 
 
+reset_game()
 play.start_program()
 
